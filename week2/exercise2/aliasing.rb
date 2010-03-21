75cd3bd97a0c09c7c10aea8cf1528de46beabb16
Michal <michal.kuklis@gmail.com>
2010-03-21 16:11:43 -0400

    cleanup alias_method_chain

--- week2/exercise2/aliasing.rb
+++ week2/exercise2/aliasing.rb
@@ -3,9 +3,7 @@
 module RubyLearning
   module Module
     def alias_method_chain(target, feature)
- 
       aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
- 
       yield(aliased_target, punctuation) if block_given?
       
       operation_with_feature = "#{aliased_target}_with_#{feature}#{punctuation}"    
@@ -13,15 +11,13 @@ module RubyLearning
       alias_method operation_without_feature, target
       alias_method target, operation_with_feature 
           
-      case
-        when public_method_defined?(operation_without_feature)
-          public target
-        when protected_method_defined?(operation_without_feature)
-          protected target
-        when private_method_defined?(operation_without_feature)
-          private target
+      if self.public_instance_methods(false).include?(target)
+        public operation_with_feature, operation_without_feature 
+      elsif self.private_instance_methods(false).include?(target)
+        private operation_with_feature, operation_without_feature  
+      else
+        protected operation_with_feature, operation_without_feature 
       end
     end
   end
 end
-
